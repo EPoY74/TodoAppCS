5ce89778d5cc475ceb0c5744fc8bd362a54eb167
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-08-11 20:18:16 +0500

    Implemented TodoTask class for represented single  task

---
+++ src/TodoApp.Core/TodoTask.cs
@@ -0,0 +1,11 @@
+namespace TodoApp.Core
+
+
+public class TodoTask
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public bool IsCompleted { get; set; }
+    public int? ProjectId { get; set; } = null
+}
